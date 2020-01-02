f2277cd5a9dd1e288416b2baaa170ad4e4686cc6
jin.park <oodmumc2014@gmail.com>
2020-01-02 11:46:46 +0900

    극장(Theater)가 직접적으로 매표소(TicketOffice)에 접근하지 않도록 캡슐화

--- src/main/java/com/dktechin/ticket/sales/model/Theater.java
+++ src/main/java/com/dktechin/ticket/sales/model/Theater.java
@@ -20,16 +20,6 @@ public class Theater {
      * @param audience 관람객 정보
      */
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            System.out.println("audience have an invitation!!");
-            Ticket ticket = this.ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            System.out.println("audience don`t have an invitation!!");
-            Ticket ticket = this.ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        this.ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/com/dktechin/ticket/sales/model/TicketSeller.java
+++ src/main/java/com/dktechin/ticket/sales/model/TicketSeller.java
@@ -1,7 +1,6 @@
 package com.dktechin.ticket.sales.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.ToString;
 
 /**
@@ -11,5 +10,23 @@ import lombok.ToString;
 @ToString
 public class TicketSeller {
     /** * 자신이 일하는 매표소 */
-    @Getter private final TicketOffice ticketOffice;
+    private final TicketOffice ticketOffice;
+
+    /**
+     * 티켓 판매원이 관람객에게 티켓을 판매한다.
+     * @param audience 관람객 정보
+     */
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            System.out.println("audience have an invitation!!");
+            Ticket ticket = this.ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            System.out.println("audience don`t have an invitation!!");
+            Ticket ticket = this.ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            this.ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
 }
